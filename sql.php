4e47764f512b2cd1a6731c12809518173cd79888
SpaceBean43 <96753962+SpaceBean43@users.noreply.github.com>
2024-01-14 04:55:00 +0800

    sql.php

---
+++ sql.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $user   = "root";
+    $password = "";
+    $database = "2101020015_wannaindrayana";
+
+    $con    = mysqli_connect($server, $user, $password, $database);
+?>
